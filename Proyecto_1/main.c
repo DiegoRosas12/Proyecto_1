5b34d75cdd964f7d6a9323c743b4aec6648b2a18
Diego Rosas <diego@macbook-pro-de-diego.local>
2017-03-08 16:23:36 -0600

    Initial Commit

---
+++ Proyecto_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Proyecto_1
+//
+//  Created by Diego Eduardo Rosas González on 08/03/17.
+//  Copyright © 2017 Diego Eduardo Rosas González. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
